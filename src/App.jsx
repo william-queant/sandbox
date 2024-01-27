c67cb3cc53a8b87b90334959242ff8b6f3f95780
William Quéant <william.queant@gmail.com>
2024-01-28 12:34:12 +1300

    upgrade dependencies to latest to avoid warnings

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
-function App() {
-  return ("hello world");
+function Greeting({ name }) {
+  return <h1>Hello, {name}</h1>;
 }
 
-export default App;
+export default function App() {
+  return <Greeting name="world" />
+}
